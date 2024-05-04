88594fc85d45d05aa2417fbd5cbbccecc4fb4a86
Maxim Manakov <mxnxkxv@mail.ru>
2024-05-05 00:03:28 +0500

    Added image sending

--- vkOrbitelSupportBot.py
+++ vkOrbitelSupportBot.py
@@ -1,18 +1,24 @@
 import vk_api
+from vk_api import VkUpload
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
 
 def write_message(sender, message):
-    auth.method('messages.send', {'user_id': sender, 'message': message, 'random_id': get_random_id()})
+    auth.method('messages.send', {'user_id': sender, 'message': message, 'random_id': get_random_id(), 'attachment': ','.join(attachments)})
 
-token = "vk1.a.xDPEyahppKoJfsXgcvQqYnfc80R0wC_TQvFrRhUijDjqgOwqK5N1uiiHkQKRm4zLmtducQNJEZNwLeEQ53uDCLYl-JMMJ6oN1Xtl16cWJJQVETapxv7ngWd0Ek14IF027XZP5-eIKbxQZ4VmiKUgUnisj0BMS9gbuyKDai-1ZJzsEWPbUiUG45jBMRqBj3Yf8ykuBDnJ3_NJZYwN_7qsww"
+token = "vk1.a.ToejyP7iQm45rcZS19zq5NsPYIsEvcGAxbKywNB0kDhC1SwNDJkBAWq0nSRxIwteJcRci9qm8KHGR9ugVTTjL0QgpvZ4CWdhYp7etkNGpGiy_6h-R71HsGxImys4iSqbPXYSH1Y11GxMBmdg27vR64QrElPnsyZch9PlIJ1knqmC3h24rmWcbzXX_CZt2viJzFcqhyG8BzEgCLbVlD-gWw"
+image = "C:/Users/mxnxkxv/Desktop/logo.jpg"
 auth = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(auth)
+upload = VkUpload(auth)
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
         received_message = event.text
         sender = event.user_id
+        attachments = []
+        upload_image = upload.photo_messages(photos=image)[0]
+        attachments.append('photo{}_{}'.format(upload_image['owner_id'], upload_image['id']))
         if received_message == "Привет":
             write_message(sender, "Добрый день!")
         elif received_message == "Пока":
